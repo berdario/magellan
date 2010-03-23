3a4c302ab080af30c5ff45e400318c825237abdf
Dario Bertini <berdario@gmail.com>
2010-03-23 19:38:49 +0100

    First somewhat useful version of context.py

--- context.py
+++ context.py
@@ -1,68 +1,94 @@
-from netsource import Network, SmartNetwork
+from netsource import Network
 from functools import partial
+from time import sleep
 
-def ciao():
-	print "ciao"
 
-network=Network()
-snetwork=SmartNetwork()
+class State(dict):
+	"Main Magellan class, holding the status of the active contexts and rules"
+	
+	def __new__(cls, *p, **k):
+		if not '_singleton' in cls.__dict__:
+			cls._singleton = dict.__new__(cls)
+		return cls._singleton
+	
+	def __init__(self):
+		self.current_context = self['Default']
+		self.THRESHOLD = 0.6
+			
+	def __getitem__(self,key):
+		try:
+			return dict.__getitem__(self,key)
+		except KeyError: #used to avoid explicitly creating a new Context object each time
+			new_context = State.Context(self)
+			self[key] = new_context
+			return new_context
+	
+	def checkmax(self):
+		"Select the more likely context based on the rules, and executes its input and output actions if needed"
+		max_context = max(self.values(), key= lambda m:m.score)
+		if True:#max_context is not self.current_context:
+			if max_context.score < self.THRESHOLD: 
+				if self.current_context is self['Default']:
+					return #if the max_context changed but both the old and the new one where under the threshold it's useless to do any action
+				max_context = self['Default']
 
-class State(object):
+			for action in self.current_context.out_actions:
+				action()
+			self.current_context = max_context
+			for action in self.current_context.in_actions:
+				action()
+
+	def add_rule(self,source,value,context,confidence):
+		"Add a rule to its context's rules and connects it to the event source"
+		rule = partial(State.Rule(confidence),self[context],value)
+		self[context].rules.append((source,rule))
+		source(rule)
 	
 		
-		
 	class Context(object):
-		def __init__(self):
+		"Context class: holds its current score, and the rules and actions related to this context"
+		def __init__(self, state):
 			self.__dict__['score'] = None
+			self.rules = []
 			self.in_actions = []
 			self.out_actions = []
+			self._state = state
 		def __setattr__(self,name,value):
 			self.__dict__[name]=value
 			if name == 'score':
-				State.checkmax()
+				self._state.checkmax() #every time the score is updated the most likely context is recalculated, but now the score is updated only in one place, so it's not very useful
+		def update_score(self):
+			"Update this context score based on individual rules confidences:\nscore= 1-((1-val1)*(1-val2)*(1-val3)...)"
+			self.score = 1-reduce(float.__mul__,(1-rule.func.confidence for source,rule in self.rules if rule.func.match),1.0)
 	
-	contexts = {'Default':Context()}
-	current_context = 'Default'
-	THRESHOLD = 0.6
 	
-	@classmethod
-	def checkmax(cls):
-		max_context_name=max(cls.contexts,key=lambda m:cls.contexts[m].score)
-		max_context=cls.contexts[max_context_name]
-		if max_context_name is not cls.current_context and max_context.score >= cls.THRESHOLD:
-			print 'contesto con score piu\' alto:',max_context_name,'contesto corrente',cls.current_context,'punteggio',max_context.score
-			for action in cls.contexts[cls.current_context].out_actions:
-				action()
-			cls.current_context = max_context_name
-			for action in max_context.in_actions:
-				action()
-			
-	contexts['Casa']=Context()
-	contexts['Casa'].in_actions.append(ciao)
+	class Rule(object):
+		"Rule class: keeps trace of its confidence value and the current state (matched or not)\nif called checks if the state changed and, if so, prompts the context to update its score"
+		def __init__(self,confidence):
+			self.match = False
+			self.confidence = confidence
+		def __call__(self,context,value,values):
+			match = value in values
+			if self.match is not match:
+				self.match = match
+				context.update_score()
 
 
+def in_casa():
+	print "sono a casa"
+	
+def out_casa():
+	print "esco di casa"
+	
+def default():
+	print "contesto di default"
 
-	@staticmethod	
-	def add_rule(source,value,context,confidence):
-		source(partial(State.rule,value,context,confidence))
-		
-	@classmethod	
-	def rule(cls,value,context,confidence,ssids):
-		if value in ssids:
-			cls.contexts[context].score=confidence
-			
-	@staticmethod		
-	def add_dumb(source,value,context,confidence):
-		source(partial(State.dumbrule,context,confidence),value)
-		
-	@classmethod
-	def dumbrule(cls,context,confidence):
-		cls.contexts[context].score=confidence
-
-		
-		
 if __name__ == '__main__':
-	#State.add_rule(network.connect_ssid,'wlan-ap','Casa',0.8)
-	State.add_dumb(snetwork.connect_ssid,'wlan-ap','Casa',0.8)
-		
-	
+	network=Network()
+	state = State()
+	state['Casa'].in_actions.append(in_casa)
+	state.add_rule(network.connect_ssid,'wlan-ap','Casa',0.8)
+	state['Casa'].out_actions.append(out_casa)
+	state['Default'].in_actions.append(default)
+	while True:
+		sleep(10)
--- netsource.py
+++ netsource.py
@@ -1,16 +1,18 @@
 from time import sleep
 from threading import Thread
-from functools import partial
 import sys
 
 sys.path.append("./cnetworkmanager.librarize")
 from networkmanager.networkmanager import NetworkManager
 
 class Network():
+	"This class spawn a thread and fetch changes to the network using the NetworkManager DBUS API, calling the handlers provided when needed"
+
 	def __init__(self):
 		self.nm = NetworkManager()
 		self.handlers = []
 		self.p=Thread(target=self.run)
+		self.p.daemon = True #simple solution, but it's better to be careful: http://joeshaw.org/2009/02/24/605
 		self.p.start()
 		
 	def get_macaddresses(self):
@@ -27,6 +29,7 @@ class Network():
 		return result
 
 	def connect_ssid(self, handler):
+		"Adds the handler to the functions to be called when an event happens or a change is detected"
 		self.handlers.append(handler)
 
 	def run(self):
@@ -35,49 +38,5 @@ class Network():
 			for f in self.handlers:		
 				f(ssids)
 			sleep(2)
-
-class SmartNetwork():
-	def __init__(self):
-		self.nm = NetworkManager()
-		self.handlers = {}
-		self.p=Thread(target=self.run)
-		self.p.start()
-		
-	def get_macaddresses(self):
-		return self._getProperty('HwAddress')
-
-	def get_ssids(self):
-		return self._getProperty('Ssid')
-
-	def _getProperty(self, property):
-		devices = self.nm.GetDevices()
-		result = []
-		for dev in filter(lambda d: d._settings_type() == "802-11-wireless", devices):
-			result.extend(str(ap[property]) for ap in dev.GetAccessPoints())
-		return result
-
-	def connect_ssid(self, handler, value):
-		self.handlers[handler] = value
-
-	def run(self):
-		while True:
-			ssids = self.get_ssids()
-			for f in self.handlers:	
-				if self.handlers[f] in ssids:
-					f()
-			sleep(2)
-
-
-	
-
-
-
-#add_rule(network.connect_ssid,'unibgfreenet','in uni',0.8)
-
-#partial(network.connect_ssid,)
-
-
-#network.connect_ssid(university_rule)
-#network.connect_ssid(home_rule)
 
 
