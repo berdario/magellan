e5996f955cec82d8eb4cf1581511d76c8a5d9686
Giuseppe Capizzi <giuseppe@silverfish.local>
2010-03-27 15:40:09 +0100

    Context cleanup

--- src/context.py
+++ src/context.py
@@ -3,15 +3,12 @@
 class Context(object):
     '''Context class: holds its current score, and the rules and actions related to this context'''
     def __init__(self, state):
-        self.__dict__['score'] = None
+        self.score = None
         self.rules = []
         self.in_actions = []
         self.out_actions = []
         self._state = state
-    def __setattr__(self, name, value):
-        self.__dict__[name] = value
-        if name == 'score':
-            self._state.checkmax() #every time the score is updated the most likely context is recalculated, but now the score is updated only in one place, so it's not very useful
     def update_score(self):
         '''Update this context score based on individual rules confidences:\nscore= 1-((1-val1)*(1-val2)*(1-val3)...)'''
         self.score = 1 - reduce(float.__mul__, (1 - rule.func.confidence for source, rule in self.rules if rule.func.match), 1.0)
+        self._state.checkmax()
