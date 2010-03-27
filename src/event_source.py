46aa6f5c105a6cc4eba94e5edb51ca86f7fa8ae2
Giuseppe Capizzi <giuseppe@silverfish.local>
2010-03-27 15:27:46 +0100

    code cleanup and refactoring

--- src/event_source.py
+++ src/event_source.py
@@ -2,20 +2,20 @@ class EventSource():
     def __init__(self):
         self._rules = []
 
-    def attach(self, observer):
-        if not observer in self._rules:
-            self._rules.append(observer)
+    def attach(self, rule):
+        if not rule in self._rules:
+            self._rules.append(rule)
 
-    def detach(self, observer):
+    def detach(self, rule):
         try:
-            self._rules.remove(observer)
+            self._rules.remove(rule)
         except ValueError:
             pass
 
     def notify(self, modifier=None):
-        for observer in self._rules:
-            if modifier != observer:
-                observer.update(self.data())
+        for rule in self._rules:
+            if modifier != rule:
+                rule.update(self.data())
                 
     def data(self):
         raise NotImplementedError
